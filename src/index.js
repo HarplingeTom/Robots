98090c4790290c994d3c62e7922a4b9bae428343
Tom Skinner <r.tom.skinner@gmail.com>
2018-10-08 19:39:46 -0700

    removed logger in index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import thunkMiddleware from 'redux-thunk';
-import { createLogger } from 'redux-logger';
 import 'tachyons';
 
 import App from './containers/App';
@@ -12,15 +11,13 @@ import { requestRobots, searchRobots } from './reducers'
 
 import './index.css';
 
-const logger = createLogger()
-
-const rootReducers = combineReducers({requestRobots, searchRobots})
+const rootReducers = combineReducers({ requestRobots, searchRobots })
 
 const store = createStore(rootReducers, applyMiddleware(thunkMiddleware, logger))
 
 ReactDOM.render(
-  <Provider store={store}>
-    <App/>
+  <Provider store={ store }>
+    <App />
   </Provider>,
   document.getElementById('root')
 );
